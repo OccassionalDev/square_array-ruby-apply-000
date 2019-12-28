1202bac583418b2b47de874f8d48e85cc845b6fc
Erica Schimmel <ericaschimmel01@gmail.com>
2019-12-28 16:26:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,21 +1,8 @@
 def square_array(array)
   # your code here
   index = 0
-  
-  #.each version
   array.each do |number|
     array[index] = number*number
     index += 1
   end
 end
-
-#.collect version
-def square_array_collect(array)
-  array.collect do |number|
-    number = number*number
-  end
-end
-
-array = [2, 4, 6, 8]
-square_array_collect(array)
-puts array
